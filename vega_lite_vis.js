d295377df51091bbfb48ee65419d785c2e7bbc3f
ChenTian22 <142318577+ChenTian22@users.noreply.github.com>
2023-10-11 20:03:00 +0800

    Add files via upload

---
+++ vega_lite_vis.js
@@ -0,0 +1,24 @@
+var vg_1 = "map_chart.vg.json";
+vegaEmbed("#map_chart", vg_1).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_2 = "bump_chart.vg.json";
+vegaEmbed("#bump_chart", vg_2).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_3 = "bar_chart.vg.json";
+vegaEmbed("#bar_chart", vg_3).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_4 = "scatter_chart.vg.json";
+vegaEmbed("#scatter_chart", vg_4).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);
+
+var vg_5 = "small_multi.vg.json";
+vegaEmbed("#small_multi", vg_5).then(function(result) {
+    // Access the Vega view instance (https://vega.github.io/vega/docs/api/view/) as result.view
+}).catch(console.error);

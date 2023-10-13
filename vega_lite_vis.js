9dd68129600f8fbde204dc2631dc27a7364d509f
ChenTian22 <142318577+ChenTian22@users.noreply.github.com>
2023-10-13 21:46:12 +0800

    Add files via upload

--- vega_lite_vis.js
+++ vega_lite_vis.js
@@ -1,24 +1,24 @@
 var vg_1 = "map_chart.vg.json";
-vegaEmbed("#map_chart", vg_1).then(function(result) {
+vegaEmbed("#map_chart", vg_1, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_2 = "bump_chart.vg.json";
-vegaEmbed("#bump_chart", vg_2).then(function(result) {
+vegaEmbed("#bump_chart", vg_2, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_3 = "bar_chart.vg.json";
-vegaEmbed("#bar_chart", vg_3).then(function(result) {
+vegaEmbed("#bar_chart", vg_3, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
 
 var vg_4 = "scatter_chart.vg.json";
-vegaEmbed("#scatter_chart", vg_4).then(function(result) {
+vegaEmbed("#scatter_chart", vg_4, {"actions": false}).then(function(result) {
     
 }).catch(console.error);
 
 var vg_5 = "small_multi.vg.json";
-vegaEmbed("#small_multi", vg_5).then(function(result) {
+vegaEmbed("#small_multi", vg_5, {"actions": false}).then(function(result) {
 
 }).catch(console.error);
